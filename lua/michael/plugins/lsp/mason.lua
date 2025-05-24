41ef5184e69dce85a2e7edb72e5143f2d124dba7
MichaelCSnyder <msnyder1992@gmail.com>
2025-05-23 23:37:59 -0400

    Working!

--- lua/michael/plugins/lsp/mason.lua
+++ lua/michael/plugins/lsp/mason.lua
@@ -4,13 +4,27 @@ return {
     dependencies = {
         "williamboman/mason-lspconfig.nvim",
     },
-    opts = {
-        ui = {
+
+    config = function()
+        local mason = require("mason") 
+        local mason_lspconfig = require("mason-lspconfig")
+
+        mason.setup({
+          ui = {
             icons = {
-                package_installed = "✓",
-                package_pending = "➜",
-                package_uninstalled = "✗"
-            }
+              package_installed = "✓",
+              package_pending = "➜",
+              package_uninstalled = "✗",
+            },
+          },
+        })
+
+      mason_lspconfig.setup({
+        ensure_installed = {
+          "ts_ls",
+          "html",
+          "cssls",
         }
-    }
+      })
+    end
 }
