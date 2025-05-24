a4d6a3125580719b0317477bfce691908668f610
MichaelCSnyder <msnyder1992@gmail.com>
2025-05-24 07:14:33 -0400

    No errors at this point trying to configure lspconfig

--- lua/michael/plugins/lsp/lspconfig.lua
+++ lua/michael/plugins/lsp/lspconfig.lua
@@ -1,8 +1,28 @@
 return {
-    "mason-org/mason-lspconfig.nvim",
-    opts = {},
-    dependencies = {
-        { "mason-org/mason.nvim", opts = {} },
-        "neovim/nvim-lspconfig",
-    },
+  "neovim/nvim-lspconfig",
+  event = { "BufReadPre", "BufNewFile" },
+  dependencies = {
+    "hrsh7th/cmp-nvim-lsp",
+    { "antosha417/nvim-lsp-file-operations", config = true },
+    { "folke/neodev.nvim", opts = {} },
+  },
+  config = function()
+    -- import lspconfig plugin
+    local lspconfig = require("lspconfig")
+
+    -- import mason_lspconfig plugin
+    local mason_lspconfig = require("mason-lspconfig")
+
+    -- import cmp-nvim-lsp plugin
+    local cmp_nvim_lsp = require("cmp_nvim_lsp")
+
+    local keymap = vim.keymap -- for conciseness
+
+    vim.api.nvim_create_autocmd("LspAttach", {
+      group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+      callback = function(ev)
+      end,
+    })
+
+  end,
 }
