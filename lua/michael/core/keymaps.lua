2ca2de2fee4ff3abfe1b21c9e80f3a1c8262117f
MichaelCSnyder <msnyder1992@gmail.com>
2025-05-23 21:44:32 -0400

    Working before LSP attempt

--- lua/michael/core/keymaps.lua
+++ lua/michael/core/keymaps.lua
@@ -1,13 +1,18 @@
 -- set leader key to space
 vim.g.mapleader = " "
 
+vim.opt.startofline = true
+
 local keymap = vim.keymap -- for conciseness
 
----------------------
--- General Keymaps -------------------
 
--- use jk to exit insert mode
-keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+vim.keymap.set("n", "<leader>d", "\"_d")
+vim.keymap.set("n", "<leader>p", "\"0p")
+vim.keymap.set("v", "<leader>d", "\"_d")
+vim.keymap.set("v", "<leader>p", "\"0p")
 
 -- clear search highlights
 keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
@@ -15,18 +20,28 @@ keymap.set("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 -- delete single character without copying into register
 -- keymap.set("n", "x", '"_x')
 
--- increment/decrement numbers
-keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
-
--- window management
-keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
-
+-- tab management
 keymap.set("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-keymap.set("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-keymap.set("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+-- keymap.set("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
+keymap.set("n", "<C-k>", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
+keymap.set("n", "<C-j>", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
+keymap.set("n", "<A-k>", "<cmd>+tabm<CR>", { desc = "Move tab to the right" }) --  go to previous tab
+keymap.set("n", "<A-j>", "<cmd>-tabm<CR>", { desc = "Move tab to the left" }) --  go to previous tab
 keymap.set("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+
+-- Search and replace word under cursor
+vim.keymap.set("n", "<leader>s", function()
+  local word = vim.fn.expand("<cword>")
+  return string.format(":%s/%s/%s/gI", "%s", "\\<" .. word .. "\\>", word)
+end, { expr = true })
+
+-- highlight yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  pattern = "*",
+  callback = function()
+    vim.highlight.on_yank {
+      higroup = "IncSearch",
+      timeout = 200,
+    }
+  end,
+})
