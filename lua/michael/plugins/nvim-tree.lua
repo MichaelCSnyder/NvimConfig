b82dcdd198cab53a264cce4cfb7249cfc98e03c9
MichaelCSnyder <msnyder1992@gmail.com>
2025-06-01 18:51:42 -0400

    Reenabling netrw

--- lua/michael/plugins/nvim-tree.lua
+++ lua/michael/plugins/nvim-tree.lua
@@ -5,9 +5,9 @@ return {
   config = function()
     local nvimtree = require("nvim-tree")
 
-    -- recommended settings from nvim-tree documentation
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
+    -- recommended settings from nvim-tree documentation. Seems to remove netrw. Not cool.
+    -- vim.g.loaded_netrw = 1
+    -- vim.g.loaded_netrwPlugin = 1
 
     nvimtree.setup({
       view = {
