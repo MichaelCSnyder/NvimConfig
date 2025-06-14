39ebf64ee81f618012fdea023956f8c7def73b6b
MichaelCSnyder <msnyder1992@gmail.com>
2025-06-14 10:46:52 -0400

    adding a formatting keybinding

--- lua/michael/plugins/formatting.lua
+++ lua/michael/plugins/formatting.lua
@@ -2,17 +2,33 @@ return {
 	"stevearc/conform.nvim",
 	opts = {},
 	config = function()
-		require("conform").setup({
+		local conform = require("conform")
+
+		conform.setup({
 			formatters_by_ft = {
-				lua = { "stylua" },
 				-- Conform will run multiple formatters sequentially
 				-- Conform will run the first available formatter
 				javascript = { "prettierd" },
+				typescript = { "prettierd" },
+				javascriptreact = { "prettierd" },
+				typescriptreact = { "prettierd" },
+				css = { "prettierd" },
+				html = { "prettierd" },
+				json = { "prettierd" },
+				markdown = { "prettierd" },
+				lua = { "stylua" },
 			},
 			format_on_save = {
 				lsp_fallback = true,
 				timeout_ms = 500,
 			},
 		})
+		vim.keymap.set("n", "<leader>mp", function()
+			conform.format({
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			})
+		end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
